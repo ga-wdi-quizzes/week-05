e5f81c0a7737866ef5858ce87cf16b5b906cfb43
Woo Yun <hh747hh@gmail.com>
2016-03-14 06:43:32 -0400

    completed

---
+++ app.rb
@@ -0,0 +1,20 @@
+
+
+describe Apartment do
+  describe "#add_tenant" do
+    subject(:apartment) do
+      apartment = Apartment.create(num_beds: 3)
+      # we start with 2 tenants (3 bedrooms)
+      apartment.add_tenant("alice")
+      apartment.add_tenant("bob")
+      apartment # return the apartment
+    end
+
+    context "when there is room (<= the number of beds)" do
+      it "adds a tenant" do
+        apartment.add_tenant("Third tenant")
+        expect(apartment.tenants.count).to eq(3)
+      end
+    end
+  end
+end
